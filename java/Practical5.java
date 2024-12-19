63f145396b505c268c4d3525c6a65b21539a7203
shivjeet1 <shivamlavhalepatil@gmail.com>
2024-12-19 08:57:08 +0530

    bubble-sort

--- java/Practical5.java
+++ java/Practical5.java
@@ -4,29 +4,23 @@ public class Practical5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Take input for the array size
         System.out.print("Enter the number of elements: ");
         int n = scanner.nextInt();
         int[] array = new int[n];
 
-        // Input array elements
         System.out.println("Enter the elements:");
         for (int i = 0; i < n; i++) {
             array[i] = scanner.nextInt();
         }
 
-        // Print the original array
         System.out.println("Original Array:");
         printArray(array);
 
-        // Sort the array using bubble sort
         bubbleSort(array);
 
-        // Print the sorted array
         System.out.println("Sorted Array:");
         printArray(array);
 
-        // Close the scanner
         scanner.close();
     }
 
@@ -45,7 +39,6 @@ public class Practical5 {
         }
     }
 
-    // Method to print the array
     public static void printArray(int[] array) {
         for (int num : array) {
             System.out.print(num + " ");
