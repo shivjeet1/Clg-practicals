25d1df4a28d9446cee5747410e189aafe82051fc
shivjeet1 <shivamlavhalepatil@gmail.com>
2024-12-19 09:10:41 +0530

    methodoverloading

--- java/Practical4.java
+++ java/Practical4.java
@@ -1,16 +1,13 @@
 public class Practical4 {
 
-    // Method to add two integers
     public int add(int a, int b) {
         return a + b;
     }
 
-    // Method to add three integers
     public int add(int a, int b, int c) {
         return a + b + c;
     }
 
-    // Method to add two doubles
     public double add(double a, double b) {
         return a + b;
     }
